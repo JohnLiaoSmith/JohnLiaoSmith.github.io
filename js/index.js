7549cb1aebddf8de4e3cd68167fc3727860460d2
JohnLiaoSmith <john.liao.smith@gmail.com>
2023-10-02 22:16:26 +0800

    third commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
+let locationsObject = "";
+fetch('assets/locations.json')
+    .then((res) => res.text())
+    .then((text) => {
+        locationsObject = JSON.parse(text);
+    })
+    .catch((e) => console.error(e));
+
+console.log(locationsObject);
+
 document.addEventListener("DOMContentLoaded", function () {
 
-    $.getJSON("assets/locations.json", function (data) {
-        console.log(data);
-    });
-    document.getElementById('ok').textContent = 'hello ssssworld';
+    document.getElementById('ok').textContent = 'hello world';
 });
