b0ff5fad20236bcd333270fe040b0af9df4f2a87
JohnLiaoSmith <john.liao.smith@gmail.com>
2023-10-02 22:29:52 +0800

    fifth commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,54 @@
-let locationsObject = "";
-fetch('assets/locations.json')
+document.addEventListener("DOMContentLoaded", function () {
+    let tableBody = document.getElementById("table-body");
+
+    fetch('assets/locations.json')
     .then((res) => res.text())
     .then((text) => {
-        locationsObject = JSON.parse(text);
-        console.log(locationsObject);
+        let locationArray = JSON.parse(text);
+        console.log(locationArray);
+
+
+        for (let i = 0; i < locationArray.length; i++) {
+            // create a table row element
+            let tableRow = document.createElement('tr');
+          
+            // create a table cell (td) element
+            let tdName = document.createElement('td');
+            // listItem.classList.add('listItem');
+            // add content to table cell element
+            tdName.innerHTML = locationArray[i]["name"];
+            // append table cell to table row
+            tableRow.appendChild(tdName);
+
+            // create a table cell (td) element
+            let tdPax = document.createElement('td');
+            // listItem.classList.add('listItem');
+            // add content to table cell element
+            tdPax.innerHTML = locationArray[i]["pax"];
+            // append table cell to table row
+            tableRow.appendChild(tdPax);
+
+            // create a table cell (td) element
+            let tdDistance = document.createElement('td');
+            // listItem.classList.add('listItem');
+            // add content to table cell element
+            tdDistance.innerHTML = locationArray[i]["distance"];
+            // append table cell to table row
+            tableRow.appendChild(tdDistance);
+
+            // create a table cell (td) element
+            let tdLocation = document.createElement('td');
+            // listItem.classList.add('listItem');
+            // add content to table cell element
+            tdLocation.innerHTML = locationArray[i]["location"];
+            // append table cell to table row
+            tableRow.appendChild(tdLocation);
+          
+            // append table row to table body
+            tableBody.appendChild(tableRow);
+          }
     })
     .catch((e) => console.error(e));
-
-
-document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById('ok').textContent = 'hello world';
 });
